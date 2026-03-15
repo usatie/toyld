7de75ba29f330d8f65bac4ac4d71ba209d2c93ac
Shun Usami <usatie@gmail.com>
2026-03-15 00:56:39 -0700

    Implement magic number check in linker.py

--- python/linker.py
+++ python/linker.py
@@ -24,12 +24,19 @@ def main():
     data = None
 
     with open(input_file, 'rb') as infile, open(output_file, 'wb') as outfile:
+        # Iterate through the lines in the input file and print them to the output file
         # TODO: Check magic number
+        line = infile.readline()
+        if line != b'LINK\n':
+            print("Invalid file format: missing magic number 'LINK'", file=sys.stderr)
+            sys.exit(1)
         # TODO: Read header and populate num_segments, num_symbols, num_relocations
         # TODO: Read segments
         # TODO: Read symbols
         # TODO: Read relocations
         # TODO: Read data
+
+    with open(input_file, 'rb') as infile, open(output_file, 'wb') as outfile:
         outfile.write(infile.read())
 
 if __name__ == '__main__':
