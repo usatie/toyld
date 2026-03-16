70dc97e32c9067ef8a3f097f4b7df7c2541005a2
Shun Usami <usatie@gmail.com>
2026-03-15 20:26:27 -0700

    Fix to treat numbers in the input file as hex

--- python/linker.py
+++ python/linker.py
@@ -3,6 +3,8 @@
 import sys
 import os
 
+# All numbers in the input file are in hex, so we need to convert them from hex to int when parsing
+
 def read_next_line(f):
     # ignore empty lines and comments to get the next meaningful line
     while True:
@@ -21,8 +23,8 @@ def parse_segments(f, num_segments, segments):
             sys.exit(1)
         try:
             name, start_str, size_str, code_letter = line.split()
-            start = int(start_str)
-            size = int(size_str)
+            start = int(start_str, 16)
+            size = int(size_str, 16)
             segments.append((name.decode(), start, size, code_letter.decode()))
             print(f"Segment {i}: name={name.decode()}, start={start}, size={size}, code_letter={code_letter.decode()}")
         except ValueError:
@@ -37,8 +39,8 @@ def parse_symbols(f, num_symbols, symbols):
             sys.exit(1)
         try:
             name, value_str, seg_number_str, sym_type = line.split()
-            value = int(value_str)
-            seg_number = int(seg_number_str)
+            value = int(value_str, 16)
+            seg_number = int(seg_number_str, 16)
             symbols.append((name.decode(), value, seg_number, sym_type.decode()))
             print(f"Symbol {i}: name={name.decode()}, value={value}, seg_number={seg_number}, sym_type={sym_type.decode()}")
         except ValueError:
@@ -56,9 +58,9 @@ def parse_relocations(f, num_relocations, relocations):
             fields = line.split()
             loc_str, seg_number_str, ref_str, rel_type = fields[0:4]
             extra_fields = map(lambda x: x.decode(), fields[4:])  # decode any extra fields as well
-            loc = int(loc_str)
-            seg_number = int(seg_number_str)
-            ref = int(ref_str)
+            loc = int(loc_str, 16)
+            seg_number = int(seg_number_str, 16)
+            ref = int(ref_str, 16)
             relocations.append((loc, seg_number, ref, rel_type.decode(), extra_fields))
             print(f"Relocation {i}: loc={loc}, seg_number={seg_number}, ref={ref}, rel_type={rel_type.decode()}, extra_fields={extra_fields}")
         except ValueError:
@@ -106,15 +108,15 @@ def main():
     if os.path.exists(output_file):
         os.remove(output_file)
 
+    segments = []
+    symbols = []
+    relocations = []
+    data = None
     for input_file in input_files:
         # Simply copy the input file to the output file
         num_segments = 0
         num_symbols = 0
         num_relocations = 0
-        segments = []
-        symbols = []
-        relocations = []
-        data = None
 
         with open(input_file, 'rb') as infile:
             print(f"Processing input file: {input_file}")
@@ -128,7 +130,8 @@ def main():
             # Read header: 'nsegs nsyms nrels'
             line = read_next_line(infile)
             try:
-                num_segments, num_symbols, num_relocations = map(int, line.split())
+                # num are written in hex, so we need to convert them from hex to int
+                num_segments, num_symbols, num_relocations = map(lambda x: int(x, 16), line.split())
                 print(f"Header: num_segments={num_segments}, num_symbols={num_symbols}, num_relocations={num_relocations}")
             except ValueError:
                 print("Invalid header format: expected three integers", file=sys.stderr)
@@ -156,14 +159,15 @@ def main():
         outfile.write(b'LINK\n')
         outfile.write(f"{num_segments} {num_symbols} {num_relocations}\n".encode())
         for name, start, size, code_letter in segments:
-            outfile.write(f"{name} {start} {size} {code_letter}\n".encode())
+            # we need to convert int back to hex when writing to the output file
+            outfile.write(f"{name} {start:x} {size:x} {code_letter}\n".encode())
         if not SKIP_SYMBOLS:
             for name, value, seg_number, sym_type in symbols:
-                outfile.write(f"{name} {value} {seg_number} {sym_type}\n".encode())
+                outfile.write(f"{name} {value:x} {seg_number:x} {sym_type}\n".encode())
         if not SKIP_RELOCATIONS:
             for loc, seg_number, ref, rel_type, extra_fields in relocations:
                 extra_str = ' '.join(extra_fields)
-                outfile.write(f"{loc} {seg_number} {ref} {rel_type} {extra_str}\n".encode())
+                outfile.write(f"{loc:x} {seg_number:x} {ref:x} {rel_type} {extra_str}\n".encode())
         if not SKIP_DATA and data is not None:
             outfile.write(data.hex().encode())
             outfile.write(b'\n') # newline to indicate the end of the data section
