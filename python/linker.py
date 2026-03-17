94894f60d0d559f5f5785ea37098fffa541bb0a5
Shun Usami <usatie@gmail.com>
2026-03-16 17:35:09 -0700

    Refactor allocator

--- python/linker.py
+++ python/linker.py
@@ -190,7 +190,7 @@ def parse_objects(input_files, SKIP_SYMBOLS=False, SKIP_RELOCATIONS=False, SKIP_
 def roundup(size, alignment):
     return (size + alignment - 1) // alignment * alignment
 
-def allocate_storage(objs):
+def allocate_storage(objs, enable_common_block):
     # Find all common blocks
     commons = {}
     for o in objs:
@@ -204,10 +204,11 @@ def allocate_storage(objs):
 
     # start text segment at 0x1000 to leave some space for the header
     TEXT_START = 0x1000
-    segment_groups = {
-            'RP': {'.text': Segment('.text', 0, 0, 'RP')}, # read-only and present, code
-            'RWP': {'.data': Segment('.data', 0, 0, 'RWP')}, # read-write and present, data
-            'RW': {'.bss': Segment('.bss', 0, 0, 'RW')} # read-write and not present, uninitialized data
+    VALID_SEGMENT_TYPES = {'RP', 'RWP', 'RW'}
+    groups = {
+        '.text': Segment('.text', 0, 0, 'RP'),
+        '.data': Segment('.data', 0, 0, 'RWP'),
+        '.bss': Segment('.bss', 0, 0, 'RW'),
     }
     WORD_ALIGNMENT = 0x0004
     PAGE_ALIGNMENT = 0x1000
@@ -215,51 +216,68 @@ def allocate_storage(objs):
     # Calculate the size of each segment group (textgroup, datagroup, bssgroup)
     for o in objs:
         for seg in o.segments:
-            if seg.code_letter not in segment_groups:
+            if seg.code_letter not in VALID_SEGMENT_TYPES:
                 print(f"Invalid code letter '{seg.code_letter}' in segment '{seg.name}' from file '{seg.filename}'", file=sys.stderr)
                 sys.exit(1)
-            if seg.name not in segment_groups[seg.code_letter]:
-                segment_groups[seg.code_letter][seg.name] = Segment(seg.name, 0, 0, seg.code_letter)
-            seg.assigned_offset = segment_groups[seg.code_letter][seg.name].size # For now, assign offset in the merged segment for now
-            segment_groups[seg.code_letter][seg.name].size += roundup(seg.size, WORD_ALIGNMENT)
+            if seg.name not in groups:
+                groups[seg.name] = Segment(seg.name, 0, 0, seg.code_letter)
+            g = groups[seg.name]
+            if g.code_letter != seg.code_letter:
+                print(f"Segment '{seg.name}' has inconsistent code letters: '{g.code_letter}' and '{seg.code_letter}'", file=sys.stderr)
+                sys.exit(1)
+            
+            seg.assigned_offset = g.size # For now, assign offset in the merged segment for now
+            g.size += roundup(seg.size, WORD_ALIGNMENT)
 
     # Calculate the start address of segments in textgroup
     text_group_start = TEXT_START
     text_group_size = 0
-    for seg in segment_groups['RP'].values():
+    text_group = []
+    for seg in groups.values():
+        if seg.code_letter != 'RP': # Only allocate space for segments in the text group
+            continue
         seg.start = text_group_start + text_group_size
         text_group_size += roundup(seg.size, WORD_ALIGNMENT)
+        text_group.append(seg)
 
     # Calculate the start address of segments in datagroup
     data_group_start = roundup(text_group_start + text_group_size, PAGE_ALIGNMENT)
     data_group_size = 0 
-    for seg in segment_groups['RWP'].values():
+    data_group = []
+    for seg in groups.values():
+        if seg.code_letter != 'RWP':
+            continue
         seg.start = data_group_start + data_group_size
         data_group_size += roundup(seg.size, WORD_ALIGNMENT)
+        data_group.append(seg)
 
     # Calculate the start address of segments in bssgroup
     bss_group_start = roundup(data_group_start + data_group_size, WORD_ALIGNMENT)
     bss_group_size = 0
+    bss_group = []
     # Allocate space for common blocks at the end of the bss segment
-    if COMMON:
+    if enable_common_block:
         bss_start = bss_group_start
-        bss_size = segment_groups['RW']['.bss'].size
+        bss_size = groups['.bss'].size
         common_start = roundup(bss_start + bss_size, WORD_ALIGNMENT)
         common_size = 0
         for sym_name, sym in commons.items():
             address = roundup(common_start + common_size, WORD_ALIGNMENT)
             common_size = address + sym.value - common_start
         bss_size = common_start + common_size - bss_start
-        segment_groups['RW']['.bss'].size = bss_size
-    for seg in segment_groups['RW'].values():
+        groups['.bss'].size = bss_size
+    for seg in groups.values():
+        if seg.code_letter != 'RW':
+            continue
         seg.start = bss_group_start + bss_group_size
         bss_group_size += roundup(seg.size, WORD_ALIGNMENT)
+        bss_group.append(seg)
 
     # Now assign addresses to all segments based on the group they belong to
     for o in objs:
         for seg in o.segments:
-            seg.assigned_address = seg.assigned_offset + segment_groups[seg.code_letter][seg.name].start # Add the group start address to get the final assigned address
-    return [Segment(seg.name, seg.start, seg.size, seg.code_letter) for group in segment_groups.values() for seg in group.values()]
+            seg.assigned_address = seg.assigned_offset + groups[seg.name].start # Add the group start address to get the final assigned address
+    return [Segment(seg.name, seg.start, seg.size, seg.code_letter) for seg in (text_group + data_group + bss_group)]
 
 def main():
     if len(sys.argv) < 2:
@@ -281,7 +299,6 @@ def main():
     SKIP_SYMBOLS = args.skip_symbols
     SKIP_RELOCATIONS = args.skip_relocations
     SKIP_DATA = args.skip_data
-    COMMON = args.common
     global DEBUG
     DEBUG = args.debug
     input_files = args.input_files
@@ -296,7 +313,7 @@ def main():
 
     # Allocate Storage for .text, .data, .bss segments and assign addresses
     if len(objs) > 1:
-        out_segments = allocate_storage(objs)
+        out_segments = allocate_storage(objs, args.common)
     else:
         out_segments = objs[0].segments
     out_symbols = [sym for o in objs for sym in o.symbols]
