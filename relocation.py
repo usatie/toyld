37f16decb00831c27637cdd6fb9f58eaa20ae89a
Shun Usami <usatie@gmail.com>
2026-03-21 06:38:34 -0700

    Implement RS4 (PC-relative 4-byte symbol address) relocation type

--- relocation.py
+++ relocation.py
@@ -37,6 +37,20 @@ def _relocate_as4(rel, obj, gsymtab, out_segments, gdata):
     # Big-endian 4-byte absolute symbol address copy
     seg_data[offset:offset+4] = (ref_sym.value + addend).to_bytes(4, byteorder='big')
 
+def _relocate_rs4(rel, obj, gsymtab, out_segments, gdata):
+    tgt_lseg = obj.segments[rel.seg_number - 1]
+    sym_name = list(obj.symbols.keys())[rel.ref - 1]
+    ref_sym = gsymtab[sym_name]
+    offset = tgt_lseg.assigned_offset + rel.loc
+    seg_data = gdata[tgt_lseg.name]
+    if offset + 4 > len(seg_data):
+        print(f"Relocation out of bounds: segment '{tgt_lseg.name}', offset {offset}, seg_data length {len(seg_data)}", file=sys.stderr)
+        sys.exit(1)
+    addend = int.from_bytes(seg_data[offset:offset+4], byteorder='big', signed=True)
+    pc_relative_addr = ref_sym.value + addend - (tgt_lseg.assigned_address + rel.loc + 4)
+    # Big-endian 4-byte absolute symbol address copy
+    seg_data[offset:offset+4] = pc_relative_addr.to_bytes(4, byteorder='big', signed=True)
+
 def _relocate_one(rel, obj, gsymtab, out_segments, gdata):
     if rel.rel_type == 'A4':
         return _relocate_a4(rel, obj, gsymtab, out_segments, gdata)
@@ -44,6 +58,8 @@ def _relocate_one(rel, obj, gsymtab, out_segments, gdata):
         return _relocate_r4(rel, obj, gsymtab, out_segments, gdata)
     elif rel.rel_type == 'AS4':
         return _relocate_as4(rel, obj, gsymtab, out_segments, gdata)
+    elif rel.rel_type == 'RS4':
+        return _relocate_rs4(rel, obj, gsymtab, out_segments, gdata)
     else:
         print(f"Unsupported relocation type: {rel.rel_type}", file=sys.stderr)
         sys.exit(1)
