a6c4be0dc2d3d010aa6b4c513f5a3b50dfdb0aa5
Shun Usami <usatie@gmail.com>
2026-03-21 09:39:50 -0700

    Refactor relocation

--- relocation.py
+++ relocation.py
@@ -1,29 +1,32 @@
 import sys
 
+def _resolve_symbol_addr(rel, obj, gsymtab, seg_data, offset):
+    sym_name = list(obj.symbols.keys())[rel.ref - 1]
+    ref_sym = gsymtab[sym_name]
+    # NOTE: Probably it's safer to read the addend from the original segment data, not from the already modified data
+    # because seg_data may have been modified by previous relocations.
+    addend = int.from_bytes(seg_data[offset:offset+4], byteorder='big', signed=True)
+    return ref_sym.value + addend
 
-def _relocate_a4(rel, obj, gsymtab, seg_data, offset):
+def _relocate_a4(rel, obj, seg_data, offset):
     ref_lseg = obj.segments[rel.ref - 1]
     # Big-endian 4-byte absolute address copy
     seg_data[offset:offset+4] = (ref_lseg.assigned_address).to_bytes(4, byteorder='big')
 
-def _relocate_r4(rel, obj, gsymtab, seg_data, offset, tgt_lseg):
+def _relocate_r4(rel, obj, seg_data, offset, tgt_lseg):
     ref_lseg = obj.segments[rel.ref - 1]
-    # Big-endian 4-byte pc-relative address copy
     pc_relative_addr = ref_lseg.assigned_address - (tgt_lseg.assigned_address + rel.loc + 4)
+    # Big-endian 4-byte pc-relative address copy
     seg_data[offset:offset+4] = pc_relative_addr.to_bytes(4, byteorder='big')
 
 def _relocate_as4(rel, obj, gsymtab, seg_data, offset):
-    sym_name = list(obj.symbols.keys())[rel.ref - 1]
-    ref_sym = gsymtab[sym_name]
-    addend = int.from_bytes(seg_data[offset:offset+4], byteorder='big', signed=True)
+    abs_addr = _resolve_symbol_addr(rel, obj, gsymtab, seg_data, offset)
     # Big-endian 4-byte absolute symbol address copy
-    seg_data[offset:offset+4] = (ref_sym.value + addend).to_bytes(4, byteorder='big')
+    seg_data[offset:offset+4] = abs_addr.to_bytes(4, byteorder='big')
 
 def _relocate_rs4(rel, obj, gsymtab, seg_data, offset, tgt_lseg):
-    sym_name = list(obj.symbols.keys())[rel.ref - 1]
-    ref_sym = gsymtab[sym_name]
-    addend = int.from_bytes(seg_data[offset:offset+4], byteorder='big', signed=True)
-    pc_relative_addr = ref_sym.value + addend - (tgt_lseg.assigned_address + rel.loc + 4)
+    abs_addr = _resolve_symbol_addr(rel, obj, gsymtab, seg_data, offset)
+    pc_relative_addr = abs_addr - (tgt_lseg.assigned_address + rel.loc + 4)
     # Big-endian 4-byte absolute symbol address copy
     seg_data[offset:offset+4] = pc_relative_addr.to_bytes(4, byteorder='big', signed=True)
 
@@ -35,9 +38,9 @@ def _relocate_one(rel, obj, gsymtab, gdata):
         print(f"Relocation out of bounds: segment '{tgt_lseg.name}', offset {offset}, seg_data length {len(seg_data)}", file=sys.stderr)
         sys.exit(1)
     if rel.rel_type == 'A4':
-        return _relocate_a4(rel, obj, gsymtab, seg_data, offset)
+        return _relocate_a4(rel, obj, seg_data, offset)
     elif rel.rel_type == 'R4':
-        return _relocate_r4(rel, obj, gsymtab, seg_data, offset, tgt_lseg)
+        return _relocate_r4(rel, obj, seg_data, offset, tgt_lseg)
     elif rel.rel_type == 'AS4':
         return _relocate_as4(rel, obj, gsymtab, seg_data, offset)
     elif rel.rel_type == 'RS4':
